ecc4a904ac4da955fbf9a4df5681c5d7ffc6a3a8
Ash Christopher <ashc@sandbox.localdomain>
2010-01-20 21:45:04 -0500

    Updated SASS definition and added error checking code to make sure the format is correct - if not, give an error message that means something to the user.

--- sass/management/commands/sass.py
+++ sass/management/commands/sass.py
@@ -5,6 +5,9 @@ from django.core.management.color  import no_style
 
 from commands import getstatusoutput
 
+class SassConfigException(Exception):
+    pass
+
 class Command(NoArgsCommand):
     requires_model_validation = False
     can_import_settings = True
@@ -27,5 +30,31 @@ class Command(NoArgsCommand):
         if not os.path.exists(bin):
             sys.stderr.write(self.style.ERROR('Sass binary defined by SASS_BIN does not exist: %s\n' %bin))
             return
+            
+        try:
+            sass_definitions = settings.SASS
+        except:
+            sass_definitions = ()
+            
+        for sass_def in sass_definitions:
+            try:
+                sass_name = sass_def.get('name', None)
+                sass_details = sass_def.get('details', {})
+                sass_input = sass_details.get('input', None)
+                sass_output = sass_details.get('output', None)
+                
+                # i hate generic exception message - try to give the user a meaningful message about what exactly the problem is.
+                for prop in [('name', sass_name), ('details', sass_details), ('input', sass_input), ('output', sass_output)]:
+                    if not prop[1]:
+                        raise SassConfigException('Sass \'%s\' property not defined in configuration:\n%s\n' %(prop[0], sass_def))                
+                self.process_sass(sass_name, sass_input, sass_output)
+            except SassConfigException, e:
+                sys.stderr.write(self.style.ERROR(e.message))
+            
+    def process_sass(self, name, sass_input, css_output):
+        print 'Converting %s to %s' %(sass_input, css_output)
+    
+    def get_sass_file_path(self, relative_path):
+        site_media = settings.MEDIA_ROOT
         
         
