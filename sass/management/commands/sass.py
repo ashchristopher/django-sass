363da8904a52eb4e1815f2716f835d75e8cf8b6f
Ash Christopher <ashc@sandbox.localdomain>
2010-01-27 17:57:05 -0500

    Refactored to create generic structure to describe Sass/CSS files

--- sass/management/commands/sass.py
+++ sass/management/commands/sass.py
@@ -30,39 +30,41 @@ class Command(BaseCommand):
         #     sys.stderr.write("%s\n" %(output))
         try:
             self.bin = settings.SASS_BIN
+            # test that binary defined exists
+            if not os.path.exists(self.bin):
+                sys.stderr.write(self.style.ERROR('Sass binary defined by SASS_BIN does not exist: %s\n' %bin))
+                return
         except:
             sys.stderr.write(self.style.ERROR('SASS_BIN is not defined in settings.py file.\n'))
             return
         
+        # make sure the Sass style given is valid.
         self.sass_style = kwargs.get('sass_style')
-        
-        if kwargs.get('list_sass'):
-            self.process_sass_list()
-        else:
-            self.process_sass_files()
-    
-    
-    def process_sass_list(self):
-        print 'Processing lis'
-        
-        
-    def process_sass_files(self):
-        # check the sass style if given - nested is default.
         if self.sass_style not in ('nested', 'compact', 'compressed', 'expanded'):
             sys.stderr.write(self.style.ERROR("Invalid sass style argument: %s\n") %self.sass_style)
             return
         
-        
-        # test that binary defined exists
-        if not os.path.exists(self.bin):
-            sys.stderr.write(self.style.ERROR('Sass binary defined by SASS_BIN does not exist: %s\n' %bin))
-            return
+        if kwargs.get('list_sass'):
+            self.process_sass_list()
+        else:
+            # self.process_sass_files()
+            pass
+    
+    
+    def process_sass_list(self):
+        print self.build_sass_structure()
+
             
+            
+        
+        
+    def build_sass_structure(self):
         try:
             sass_definitions = settings.SASS
         except:
             sass_definitions = ()
             
+        sass_struct = []
         for sass_def in sass_definitions:
             try:
                 sass_name = sass_def.get('name', None)
@@ -74,44 +76,53 @@ class Command(BaseCommand):
                 for prop in [('name', sass_name), ('details', sass_details), ('input', sass_input), ('output', sass_output)]:
                     if not prop[1]:
                         raise SassConfigException('Sass \'%s\' property not defined in configuration:\n%s\n' %(prop[0], sass_def))                
-                self.process_sass(sass_name, sass_input, sass_output)
             except SassConfigException, e:
                 sys.stderr.write(self.style.ERROR(e.message))
                 return
+            sass_input_root = self.get_file_path(sass_input)
+            sass_output_root = self.get_file_path(sass_output)
+            sass_struct.append({
+                'name' : sass_name,
+                'input' : sass_input_root,
+                'output' : sass_output_root,
+            })
+        return sass_struct    
             
             
-    def process_sass(self, name, sass_input, css_output):
-        """
-            The user may whish to keep their sass files in their MEDIA_ROOT directory,
-            or they may wish them to be somewhere outsite - even outside their project
-            directory. We try to support both.
-            
-            The same is true for the CSS output file. We recommend putting it in the 
-            MEDIA_ROOT but if there is a reason not to, we support that as well.
-        """
-        # if the user gives an absolute path, don't use the MEDIA_ROOT.
-        sass_input_root = sass_input if os.path.isabs(sass_input) else settings.MEDIA_ROOT + os.path.sep + sass_input
-        sass_output_root = css_output if os.path.isabs(css_output) else settings.MEDIA_ROOT + os.path.sep + css_output
-
-        # check that the sass input file actually exists.
-        if not os.path.exists(sass_input_root):
-            raise SassConfigException('The input path \'%s\' seems to be invalid.\n' %sass_input_root)
+    # def process_sass_dir(self, name, sass_input, css_output):
+    #     """
+    #         The user may whish to keep their sass files in their MEDIA_ROOT directory,
+    #         or they may wish them to be somewhere outsite - even outside their project
+    #         directory. We try to support both.
+    #         
+    #         The same is true for the CSS output file. We recommend putting it in the 
+    #         MEDIA_ROOT but if there is a reason not to, we support that as well.
+    #     """
+    #     # if the user gives an absolute path, don't use the MEDIA_ROOT.
+    #     sass_input_root = sass_input if os.path.isabs(sass_input) else settings.MEDIA_ROOT + os.path.sep + sass_input
+    #     sass_output_root = css_output if os.path.isabs(css_output) else settings.MEDIA_ROOT + os.path.sep + css_output
+    # 
+    #     # check that the sass input file actually exists.
+    #     if not os.path.exists(sass_input_root):
+    #         raise SassConfigException('The input path \'%s\' seems to be invalid.\n' %sass_input_root)
+    #     
+    #     # make sure the output directory exists.
+    #     output_path = sass_output_root.rsplit('/', 1)[0]
+    #     if not os.path.exists(output_path):
+    #         # try to create path
+    #         try:
+    #             os.mkdirs(output_path, 0644)
+    #         except os.error, e:
+    #             raise SassConfigException(e.message)
+    #         except AttributeError, e:
+    #             # we have an older version of python that doesn't support os.mkdirs - fail gracefully.
+    #             raise SassConfigException('Output path does not exist - please create manually: %s\n' %output_path)
+    #     print 'Processing %s' %name
         
-        # make sure the output directory exists.
-        output_path = sass_output_root.rsplit('/', 1)[0]
-        if not os.path.exists(output_path):
-            # try to create path
-            try:
-                os.mkdirs(output_path, 0644)
-            except os.error, e:
-                raise SassConfigException(e.message)
-            except AttributeError, e:
-                # we have an older version of python that doesn't support os.mkdirs - fail gracefully.
-                raise SassConfigException('Output path does not exist - please create manually: %s\n' %output_path)
-        print 'Processing %s' %name
         
-        
-    def get_sass_file_path(self, relative_path):
+    def get_file_path(self, path):
+        if os.path.isabs(path):
+            return path
         site_media = settings.MEDIA_ROOT
-        
+        return site_media + os.path.sep + path
         
