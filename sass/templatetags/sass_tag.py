4b8cc7017c847ab72ba9f07859bf13bb9b6685ab
Ash Christopher <ashc@sandbox.localdomain>
2010-02-20 13:32:31 -0500

    Updated the rendering of css

--- sass/templatetags/sass_tag.py
+++ sass/templatetags/sass_tag.py
@@ -4,15 +4,31 @@ from django import template
 from django.conf import settings
 from django.utils.http import urlquote
 
+from sass.utils import SassUtils
+from sass.models import SassModel
+
 
 register = template.Library()
 
 class SassNode(template.Node):
     def __init__(self, name):
         self.name = name
-    
+
+        # check to make sure the name passed into the tag is defined.
+        name_is_valid = False
+        for s in SassUtils.build_sass_structure():
+            if s['name'] == name:
+                name_is_valid = True
+                self.sass_structure = s
+                continue
+        if not name_is_valid:
+            raise template.TemplateSyntaxError('Sass name "%s" does not exist.' % self.name)
+        
+        
     def render(self, context):
-        css = "<link href='' rel='stylesheet' type='text/css' />"
+        media_url = self.sass_structure['media_out']
+        sass_obj = SassModel.objects.get(name=self.sass_structure['name'])
+        css = "<link href='%s?%s' rel='stylesheet' type='text/css' />" %(media_url, sass_obj.digest)
         return css
 
 
