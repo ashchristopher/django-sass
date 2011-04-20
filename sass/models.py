1fde489df980ab9319e5b8d4bb3f24abc018e3d8
Michael Shepanski <mjs7231@gmail.com>
2011-04-20 18:59:09 -0400

    Optional settings for SASS_ROOT and SASS_URL which default back to MEDIA_ROOT and MEDIA_URL respectivly.

--- sass/models.py
+++ sass/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.utils.http import urlquote
 
+SASS_ROOT = getattr(settings, 'SASS_ROOT', settings.MEDIA_ROOT)
+SASS_URL = getattr(settings, 'SASS_URL', settings.MEDIA_URL)
+
 
 class SassModel(models.Model):
     name = models.CharField(max_length=60, primary_key=True, help_text='Name of the Sass conversion.')
@@ -9,16 +12,17 @@ class SassModel(models.Model):
     css_path = models.CharField(max_length=255, help_text='Path to the generated CSS file.')
     style = models.CharField(choices='', max_length=10, help_text='The style used when creating the css file.')
     source_modified_time = models.CharField(max_length=12, help_text='Last time the source file was modified.')
-    
+
     def __unicode__(self):
         return self.name
-        
+
     def relative_css_path(self):
-        return self.css_path.split(settings.MEDIA_ROOT)[1].lstrip('/')
-        
+        print self.css_path.split(SASS_ROOT)[1].lstrip('/')
+        return self.css_path.split(SASS_ROOT)[1].lstrip('/')
+
     def css_media_path(self):
-        return settings.MEDIA_URL + urlquote(self.relative_css_path())
-        
+        return SASS_URL + urlquote(self.relative_css_path())
+
 
 from sass import listeners
 listeners.start_listening()
